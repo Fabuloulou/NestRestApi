2ed51278615a9126e51f1048252a1df467514116
Fabien LE BAILLIF <fabien.le.baillif@gmail.com>
2024-04-25 16:55:48 +0200

    Adding excluded table feature

--- src/multiplication-tables/trainer.helper.ts
+++ src/multiplication-tables/trainer.helper.ts
@@ -62,7 +62,12 @@ export class TrainerHelper {
     }
 
     public generateOperations(quantity = 20): [number, number][] {
-        const score = [...this.getDetailedResult(this._trainer.sessions.flatMap((sessions) => sessions.results).map((op) => this.mapOperationToDto(op)))];
+        const excludedTable = this._trainer.excludedTable ?? 9;
+        const score = [
+            ...this.getDetailedResult(this._trainer.sessions.flatMap((sessions) => sessions.results).map((op) => this.mapOperationToDto(op))).filter(
+                (op) => op.operators[0] < excludedTable,
+            ),
+        ];
         this.shuffleArray(score);
 
         const operations: [number, number][] = [];

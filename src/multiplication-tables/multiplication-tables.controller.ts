54cad0a814a1230cc90718fd37ab46a3da6437b9
Fabien LE BAILLIF <fabien.le.baillif@gmail.com>
2025-01-21 13:45:20 +0100

    Send mqtt message on end of multpilication-table session

--- src/multiplication-tables/multiplication-tables.controller.ts
+++ src/multiplication-tables/multiplication-tables.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Logger, Param, Patch } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { MqttService } from '../utils/mqtt-service';
 import { TrainerDto, TrainingScoreFullDto, TrainingSessionFullDto } from './models/multiplication-tables.dto';
 import { MultiplicationTablesService } from './multiplication-tables.service';
 
@@ -8,7 +9,7 @@ import { MultiplicationTablesService } from './multiplication-tables.service';
 export class MultiplicationTablesController {
     private readonly logger = new Logger(MultiplicationTablesController.name);
 
-    public constructor(private _service: MultiplicationTablesService) {}
+    public constructor(private _service: MultiplicationTablesService, private _mqttService: MqttService) {}
 
     @Get(':person')
     public getTrainer(@Param('person') person: string): TrainerDto {
@@ -32,6 +33,18 @@ export class MultiplicationTablesController {
     @Patch(':person')
     public addTrainingSession(@Param('person') person: string, @Body() body: TrainingSessionFullDto): TrainerDto {
         this._service.addTrainingSession(person, body);
-        return this._service.loadAndComputeScore(person);
+        const trainer = this._service.loadAndComputeScore(person);
+
+        this._mqttService.publish(
+            'multiplication-tables',
+            `${person} a terminé une session.\nScore : ${Math.round(trainer.trainerScore.totalScore)}\nScore de la session : ${Math.round(
+                trainer.sessions.pop().sessionScore,
+            )}.\nErreurs : ${body.results
+                .filter((res) => res.promptedResult !== res.operators[0] * res.operators[1])
+                .map((res) => `${res.operators[0]} x ${res.operators[1]} : ${res.promptedResult}`)
+                .join('\n')}`,
+        );
+
+        return trainer;
     }
 }
--- src/multiplication-tables/multiplication-tables.module.ts
+++ src/multiplication-tables/multiplication-tables.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MqttService } from '../utils/mqtt-service';
 import { MultiplicationTablesController } from './multiplication-tables.controller';
 import { MultiplicationTablesRepository } from './multiplication-tables.repository';
 import { MultiplicationTablesService } from './multiplication-tables.service';
 
 @Module({
-    providers: [MultiplicationTablesService, MultiplicationTablesRepository],
+    providers: [MqttService, MultiplicationTablesService, MultiplicationTablesRepository],
     controllers: [MultiplicationTablesController],
 })
 export class MultiplicationTablesModule {}
